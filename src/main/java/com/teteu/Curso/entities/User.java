ba3d3b8802c6ebe03a788f66744afbe3ebcf2672
Teteu <matheuskiper96@gmail.com>
2024-10-24 13:03:39 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/teteu/Curso/entities/User.java
+++ src/main/java/com/teteu/Curso/entities/User.java
@@ -1,11 +1,14 @@
 package com.teteu.Curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -23,6 +26,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	//OneToMany é usado quando a relação é um para muitos 
+	//(nesse caso tem que inserir o nome do atributo na classe correspondente através do mappedBy)
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();;
 	
 	public User() {
 	}
@@ -73,6 +81,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
