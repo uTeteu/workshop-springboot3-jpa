4ecf66b382c506c664e402eced4c873be6d7a0be
Teteu <matheuskiper96@gmail.com>
2024-10-25 16:08:28 -0300

    findById

--- src/main/java/com/teteu/Curso/services/UserService.java
+++ src/main/java/com/teteu/Curso/services/UserService.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import com.teteu.Curso.entities.User;
 import com.teteu.Curso.repositories.UserRepository;
+import com.teteu.Curso.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/teteu/Curso/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.teteu.Curso.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+	
+}
