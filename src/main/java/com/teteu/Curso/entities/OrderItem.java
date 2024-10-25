2bbc1b953480fdfa5a1924a773a8ef12a8282f1e
Teteu <matheuskiper96@gmail.com>
2024-10-25 14:14:37 -0300

    Subtotal e Total Methods

--- src/main/java/com/teteu/Curso/entities/OrderItem.java
+++ src/main/java/com/teteu/Curso/entities/OrderItem.java
@@ -64,6 +64,10 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -88,4 +92,5 @@ public class OrderItem implements Serializable {
 			return false;
 		return true;
 	}
+	
 }
