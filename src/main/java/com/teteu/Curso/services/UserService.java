fd043cc1bf46cdc108302594e6f04eff2ff31ecc
Teteu <matheuskiper96@gmail.com>
2024-10-25 19:18:42 -0300

    Exception handling - update

--- src/main/java/com/teteu/Curso/services/UserService.java
+++ src/main/java/com/teteu/Curso/services/UserService.java
@@ -13,6 +13,8 @@ import com.teteu.Curso.repositories.UserRepository;
 import com.teteu.Curso.services.exceptions.DatabaseException;
 import com.teteu.Curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	
 	public User update(Long id, User obj) {
 		//getReferenceById acessa o id antes de ir ao banco de dados, nesse caso as alterações ainda não serão gravadas no banco
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
