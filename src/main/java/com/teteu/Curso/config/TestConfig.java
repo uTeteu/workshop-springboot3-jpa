6e05ef6a86c711bd2cc6cc621bdc5dce7cbf7e84
Teteu <matheuskiper96@gmail.com>
2024-10-25 13:55:54 -0300

    Payment, one-to-one association

--- src/main/java/com/teteu/Curso/config/TestConfig.java
+++ src/main/java/com/teteu/Curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.teteu.Curso.entities.Category;
 import com.teteu.Curso.entities.Order;
 import com.teteu.Curso.entities.OrderItem;
+import com.teteu.Curso.entities.Payment;
 import com.teteu.Curso.entities.Product;
 import com.teteu.Curso.entities.User;
 import com.teteu.Curso.entities.enums.OrderStatus;
@@ -80,7 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 
